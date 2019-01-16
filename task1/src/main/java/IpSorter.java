ae6670277acfcb114f7b730a50f920386e11329d
mikhail <mishkamtbfever@mail.ru>
2019-01-16 22:25:41 +0300

    new algorithm
    update algorithm

--- task1/src/main/java/IpSorter.java
+++ task1/src/main/java/IpSorter.java
@@ -28,38 +28,144 @@ public class IpSorter {
         ArrayList<String> list = new ArrayList<String>();
         //192.168.0.1
         //192.168.0.5
-        if(dif4==0)
-        {int ai = begin3-1;
-        int a = fourth1;
-        int b = fourth2;
-        for (int i = dif3+1; i>0;i--)
+
+        //первая часть
+        if (dif1 != 0) {
+
+            for (int i = first1; i <= first2; i++) {
+                if (i == first1) {
+                    for (int j = second1; j < 10; j++) {
+                        if (j == second1) {
+                            for (int k = third1; k < 10; k++) {
+                                if (k == third1) {
+                                    for (int q = fourth1; q < 10; q++) {
+                                        list.add(i + "." + j + "." + k + "." + q);
+                                    }
+                                } else {
+                                    for (int q = 0; q < 10; q++) {
+                                        list.add(i + "." + j + "." + k + "." + q);
+                                    }
+                                }
+                            }
+                        } else {
+                            for (int k = 0; k < 10; k++) {
+                               for (int q = 0; q<10; q++){
+                                list.add(i + "." + j + "." + k + "." + q);}
+                            }
+                        }
+
+                    }
+                } else {
+                    if (i == first2) {
+                        for (int j = 0; j <= second2; j++) {
+                            if (j == second2) {
+                                for (int k = 0; k <= third2; k++) {
+                                    if(k == third2)
+                                    {for(int q = 0; q<=fourth2; q++){
+                                        list.add(i + "." + j + "." + k + "." + q);}
+                                    }
+                                    else {
+                                        for (int q = 0; q<10; q++){
+                                            list.add(i + "." + j + "." + k + "." + q);}
+                                    }
+                                }
+                            } else {
+                                for (int k = 0; k < 10; k++) {
+                                    for (int q = 0; q<10; q++){
+                                        list.add(i + "." + j + "." + k + "." + q);}
+                                }
+                            }
+                        }
+                    } else {
+                        for (int j = 0; j < 10; j++) {
+                            for (int k = 0; k < 10; k++) {
+                                for (int q = 0; q<10; q++){
+                                    list.add(i + "." + j + "." + k + "." + q);}
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        //вторая часть
         {
-            if(i==dif3+1){
-                for(int j = a+1; j<10; j++){
-                    list.add(first1+"."+second1+"."+ai +"."+j);}
+            if (dif2 != 0) {
+                int ai = begin2 - 1;
+                int a = third1;
+                int b = third2;
+                int a3 = begin3 + 1;
+                for (int i2 = dif2 + 1; i2 > 0; i2--) {
+                    if (i2 == dif2 + 1) {
+                        for (int j = a; j < 10; j++) {
+                            if (j == a) {
+                                for (int k4 = fourth1 + 1; k4 < 10; k4++) {
+                                    list.add(first1 + "." + ai + "." + j + "." + k4);
+                                }
+                            } else {
+                                for (int k4 = 0; k4 < 10; k4++) {
+                                    list.add(first1 + "." + ai + "." + j + "." + k4);
+                                }
+                            }
+                        }
+                    } else {
+                        if (i2 == 1) {
+                            for (int k = 0; k <= b; k++) {
+                                if (k == b) {
+                                    for (int k5 = 0; k5 < fourth2; k5++) {
+                                        list.add(first1 + "." + ai + "." + k + "." + k5);
+                                    }
+                                } else {
+                                    for (int ii = 0; ii < 10; ii++) {
+                                        list.add(first1 + "." + ai + "." + k + "." + ii);
+                                    }
+                                }
+                            }
+                        } else {
+                            for (int q = 0; q < 10; q++) {
+                                for (int k6 = 0; k6 < 10; k6++) {
+                                    list.add(first1 + "." + ai + "." + q + "." + k6);
+                                }
+                            }
+                        }
+                    }
+                    ai++;
+                    a3--;
 
+                }
             }
-            else
-            {
-                if(i==1)
-                {
-                    for (int k = 0; k<b; k++)
-                    {
-                        list.add(first1+"."+second1+"."+ai +"."+k);
+            //третья часть
+            else {
+                if (dif3 != 0) {
+                    int ai = begin3 - 1;
+                    int a = fourth1;
+                    int b = fourth2;
+                    for (int i = dif3 + 1; i > 0; i--) {
+                        if (i == dif3 + 1) {
+                            for (int j = a + 1; j < 10; j++) {
+                                list.add(first1 + "." + second1 + "." + ai + "." + j);
+                            }
+
+                        } else {
+                            if (i == 1) {
+                                for (int k = 0; k < b; k++) {
+                                    list.add(first1 + "." + second1 + "." + ai + "." + k);
+                                }
+                            } else {
+                                for (int q = 0; q < 10; q++) {
+                                    list.add(first1 + "." + second1 + "." + ai + "." + q);
+                                }
+                            }
+                        }
+                        ai++;
+                    }
+
+                } else {
+                    for (int i = fourth1 + 1; i < fourth2; i++) {
+                        list.add(first1 + "." + second1 + "." + third1 + "." + i);
                     }
                 }
-                else {
-                    for (int q = 0; q<10; q++)
-                    {
-                        list.add(first1+"."+second1+"."+ai +"."+q);
-                    }
-                }
             }
-            ai++;
         }
-
-        }
-
         return list;
     }
 
@@ -90,6 +196,8 @@ public class IpSorter {
         }
         return begin;
     }
+
+
 }
 
 
